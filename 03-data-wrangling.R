6f465bd3b833f35cc3d823778f11698df3fd49e0
eLeonBM <62080028+eLeonBM@users.noreply.github.com>
2023-02-01 10:06:57 -0600

    Add files via upload

--- 03-data-wrangling.R
+++ 03-data-wrangling.R
@@ -162,7 +162,16 @@ monthly_observations <- data %>%
 
 
 
+#Podemos usar bases de datos de R
+install.packages("nycflights13")
+library(nycflights13)
 
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
 
 
 
